10d5955e6430680c7c5f12c21ff72bcb6f76d9a2
Victorypa <victorypa1992@yandex.ru>
2019-06-02 01:37:34 +0300

    Arrow functions

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = new easyHTTP;
+// const http = new easyHTTP;
 
 // Get Posts
 // http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
@@ -23,10 +23,10 @@ const http = new easyHTTP;
 
 // CreateData
 
-const data = {
-    title: 'Custom Post',
-    body: 'This is a custom post'
-}
+// const data = {
+//     title: 'Custom Post',
+//     body: 'This is a custom post'
+// }
 
 // // Create Post
 // http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
@@ -48,10 +48,65 @@ const data = {
 
 
 // Delete post
-http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log(response);
-  }
-});
+// http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+
+// ARROW FUNCTIONS
+
+// const sayHello = function() {
+//   console.log('Hello');
+// }
+
+
+// const sayHello = () => {
+//   console.log('Hello');
+// } 
+
+
+// One line function does not need braces
+// const sayHello = () => console.log('Hello');
+
+
+// One line returns
+// const sayHello = () => 'Hello';
+
+// Same as above
+// const sayHello = function() {
+//   return 'Hello';
+// }
+
+// Return object
+// const sayHello = () => ({msg: 'Helo'});
+
+// Single param does not need parenthesis
+
+// const sayHello = name => console.log(`Hello ${name}`);
+
+// Multiple params need parenthesis
+// const sayHello = (firsName, lastName) => console.log(`Hello ${firstName} ${lastName}`);
+
+// sayHello('Brad', 'Traversy');
+
+
+const users = ['Nathan', 'John', 'William'];
+
+// const nameLengths = users.map(function(name) {
+// return name.length;
+// });
+
+// Shorter
+
+const nameLengths = users.map((name) => {
+  return name.length;
+  });
+
+  // Shortest
+  const nameLengths = users.map(name => name.length);
+
+console.log(nameLengths)
