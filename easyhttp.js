00f647126b4a851c7df2f2d2e23cf72aa9058084
Victorypa <victorypa1992@yandex.ru>
2019-06-01 13:22:29 +0300

    custom http library ajax &callbacks(part2)

--- easyhttp.js
+++ easyhttp.js
@@ -1,30 +1,62 @@
 function easyHTTP() {
     this.http = new XMLHttpRequest();
-  }
-  
-  // Make an HTTP GET Request
-  easyHTTP.prototype.get = function(url, callback) {
+}
+
+// Make an HTTP GET Request
+easyHTTP.prototype.get = function (url, callback) {
     this.http.open('GET', url, true);
+
+    let self = this;
+    this.http.onload = function () {
+        if (self.http.status === 200) {
+            callback(null, self.http.responseText);
+        } else {
+            callback('Error: ' + self.http.status);
+        }
+    }
+
+    this.http.send();
+}
+
+// Make an HTTP POST Request
+easyHTTP.prototype.post = function(url, data, callback) {
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
   
     let self = this;
     this.http.onload = function() {
-      if(self.http.status === 200) {
-        callback(null, self.http.responseText);
-      } else {
-        callback('Error: ' + self.http.status);
-      }
+      callback(null, self.http.responseText);
     }
   
-    this.http.send();
+    this.http.send(JSON.stringify(data));
   }
   
-  // Make an HTTP POST Request
+// Make an HTTP PUT Request
+
+easyHTTP.prototype.put = function(url, data, callback) {
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
   
-  // Make an HTTP PUT Request
+    let self = this;
+    this.http.onload = function() {
+      callback(null, self.http.responseText);
+    }
   
-  // Make an HTTP DELETE Request
-  
-  
-//Make an HTTP  POST Request 
-//Make an HTTP  PUT Request 
-//Make an HTTP  DELETE Request 
+    this.http.send(JSON.stringify(data));
+  }
+
+// Make an HTTP DELETE Request
+easyHTTP.prototype.delete = function (url, callback) {
+    this.http.open('DELETE', url, true);
+
+    let self = this;
+    this.http.onload = function () {
+        if (self.http.status === 200) {
+            callback(null, 'Post deleted');
+        } else {
+            callback('Error: ' + self.http.status);
+        }
+    }
+
+    this.http.send();
+}
